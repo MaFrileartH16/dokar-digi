f6493cd0eb07687e3d81a63fd273c17ce239509e
MaFrileartH16 <muhammadfauzulhanif16@gmail.com>
2025-02-16 16:50:31 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -38,6 +39,11 @@ class User extends Authenticatable
   public function setPasswordAttribute($value): void
   {
     $this->attributes['password'] = Hash::make($value);
+  }
+
+  public function socialAccounts(): HasMany
+  {
+    return $this->hasMany(SocialAccount::class);
   }
 
   protected function casts(): array
